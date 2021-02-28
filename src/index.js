761d66c255afd272dbc94acce74e87c277a96b03
Yuki MINAMIYA <y.minamiya@gmail.com>
2021-03-01 01:01:33 +0900

    33. タスクの削除機能

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ const onClickAdd = () => {
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
   });
 
   div.appendChild(li);
